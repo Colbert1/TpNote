32a98c3528c0dd383a7bce909a2e3c3b347e7f3c
Colbert1 <gcolbert@la-providence.net>
2020-01-23 11:41:49 +0100

    create log pour eleve et prof et delete log2

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
 <body>
     <div class="btn">
         <form action="login2.php" method="post">
-            <button type="button" class="btn" name="Prof">Profs</button>
-            <button type="button" class="btn" name="Eleve">Eleves</button>
-            <button type="submit" class="btn">Confirmer</button>
+            <button type="button" class="btn" name="Prof">Profs</button><!-- lien vers une page logprof.php-->
+            <button type="button" class="btn" name="Eleve">Eleves</button><!-- lien vers une page logeleve.php-->
         </form>
     </div>
 </body>
