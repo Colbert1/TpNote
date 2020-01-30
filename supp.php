99a3430689d70bdb85a5b9bb338e7c3625f3383f
Colbert1 <gcolbert@la-providence.net>
2020-01-30 11:56:46 +0100

    fdzzggz

--- supp.php
+++ supp.php
@@ -10,15 +10,14 @@ include("User.php"); ?>
     try {
         $conn = new PDO('mysql:host=192.168.65.206;dbname=noel;charset=utf8', 'Colbert', 'Colbert');
         $DonneeUser = $conn->query("SELECT * FROM Eleve");
-        $TabUserIndex = 0;
+        $TabUser = 0;
         while ($tab = $DonneeUser) {
-            $TabUser[$TabUserIndex++] = new User($tab["Id_Eleve"], $tab["Nom"]);
+            $TabUser[$TabUser++] = new User($tab["Id_Eleve"], $tab["Nom"]);
         }
-        }
-        catch(exception $e) {
+    } catch (exception $e) {
         $e->getMessage();
-        }
-       
+    }
+
     ?>
 
     <h2 class="login-title">- Suppression dans la BDD -</h2>
