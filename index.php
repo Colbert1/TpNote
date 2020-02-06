00a29ad350c07b7fe5f64e19891f9c656dea2545
Colbert1 <gcolbert@la-providence.net>
2020-02-06 11:55:31 +0100

    essai api date accueil

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php require("ConnectBDD.php");
 include('head.html') ?>
 <title>Connexion</title>
-<script type="text/javascript" src="ex.js"></script>
+<script type="text/javascript" src="heure.js"></script>
 </head>
 <!--Connexion utilisateur-->
 
 <body>
+<p id="demo"></p>
     <div>
         <button onclick="window.location.href = 'logprof.php';">Prof</button>
         <button onclick="window.location.href = 'logeleve.php';">Eleve</button>
     </div>
+
+
 </body>
 
 </html>
